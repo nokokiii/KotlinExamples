3a850b5bef183c12c01261e42966be5079bd4f71
Nokokiii <kubanokoki@gmail.com>
2024-09-24 23:46:53 +0200

    infix function

--- app/src/main/java/com/example/kotlinapplication/infixfun/InfixFunTask.kt
+++ app/src/main/java/com/example/kotlinapplication/infixfun/InfixFunTask.kt
@@ -3,13 +3,18 @@
 // Pierwszy argument z pary bedzie mówił, ile razy należy powtórzyć string,
 // a drugi, jakiego separatora należy użyć.
 
+infix fun String.repeatAndJoinWith(pair: Pair<Int, String>) : String {
+    val (times, separator) = pair;
+    return List(times) {this}.joinToString(separator)
+}
+
 fun main() {
     // TODO: Odkomentuj
-//    val result1 = "Hello" repeatAndJoinWith (3 to "-") // Powinno zwracać Hello-Hello-Hello
-//    val result2 = "Kotlin" repeatAndJoinWith (2 to " ")
-//    val result3 = "Hi" repeatAndJoinWith (5 to ", ")
-//
-//    println(result1)
-//    println(result2)
-//    println(result3)
+    val result1 = "Hello" repeatAndJoinWith (3 to "-") // Powinno zwracać Hello-Hello-Hello
+    val result2 = "Kotlin" repeatAndJoinWith (2 to " ")
+    val result3 = "Hi" repeatAndJoinWith (5 to ", ")
+
+    println(result1)
+    println(result2)
+    println(result3)
 }
